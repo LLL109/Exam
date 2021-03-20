93f26aa9172ea3ff637bed13435310bdc9d59f4f
lyf <407646459@qq.com>
2021-03-20 21:43:58 +0800

    first

---
+++ client/flask_client.py
@@ -0,0 +1,33 @@
+######################################################
+#        > File Name: flask_client.py
+#      > Author: GuoXiaoNao
+ #     > Mail: 250919354@qq.com
+ #     > Created Time: Mon 20 May 2019 11:52:00 AM CST
+ ######################################################
+
+from flask import Flask, send_file
+import sys
+
+from werkzeug.serving import make_server
+
+app = Flask(__name__)
+@app.route('/bank')
+def bank():
+    #题库界面
+    return send_file('templates/bank.html')
+@app.route('/quiz_info')
+def quiz_info():
+    #考试信息页
+    return send_file('templates/quiz_info.html')
+
+@app.route('/<quiz_id>/<page_index>/quiz')
+def quiz(quiz_id,page_index):
+    #考试界面
+    return send_file('templates/quiz.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
